6d5b20fc9082e01e79e96f9ab8ef661d6b9e2d59
KarlieB <ericakarlb@gmail.com>
2023-03-18 02:39:48 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - Last Integer
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf(n % 10)
+	return (0);
+}
