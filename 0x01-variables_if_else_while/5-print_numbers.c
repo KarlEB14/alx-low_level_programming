d79db466bfbbf2692f583d1dab274deb5297f586
KarlieB <ericakarlb@gmail.com>
2023-03-19 15:25:35 -0700

    ataya

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,7 +4,7 @@
 
 
 /**
- * main - Numbers
+ * main - print-numbers
  *
  * Return: Always 0 (Success)
  */
@@ -12,8 +12,8 @@ int main(void)
 {
 	int n;
 
-	for (n = 0; n <= 10; n++);{
+	for (n = 0; n <= 10; n++)
 		printf("%d\n", n);
-	}
+
 	return (0);
 }
